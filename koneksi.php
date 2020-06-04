978a34ca80cfda2408ca5e9797e7fc9a22bb9d68
Anisa <55769863+anisanisa9@users.noreply.github.com>
2020-06-04 22:44:04 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,15 @@
+<?php
+
+    $DB_HOST = "localhost";
+    $DB_DATABASE = "database";
+    $DB_USERNAME = "root";
+    $DB_PASSWORD = "";
+    $DB_PORT = "3306";
+
+    $db1 = new mysqli($DB_HOST,$DB_USERNAME,$DB_PASSWORD,$DB_DATABASE,$DB_PORT);
+
+    if($db1->connect_error){
+        echo "failed";
+    }
+
+?>
